281de8cf4a6598e7e7b9530d1d96b9b44b0a52bb
manojmpg114 <manojmpg114@gmail.com>
2020-10-04 19:41:30 -0400

    changed units to farenheit
    api link added &units=f, prefer seeing the whole thing in farenheit vs celsius, might add a 2nd link or do conversions on print later

--- weather-app/src/app/apixu.service.ts
+++ weather-app/src/app/apixu.service.ts
@@ -10,7 +10,7 @@ export class ApixuService {
 
   getWeather(location){
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=7f706c352ebb493c90765a3c11d2c0ea&query=' + location
+      'http://api.weatherstack.com/current?access_key=7f706c352ebb493c90765a3c11d2c0ea&units=f&query=' + location
     );
   }
 }
