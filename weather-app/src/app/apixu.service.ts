358b4c5bb9251c81da49c2279b2bb880bc5955c2
manojmpg114 <manojmpg114@gmail.com>
2020-10-04 01:21:30 -0400

    getWeather function bug fix
    issue was wrong http for API to retrieve query, uses 'query' and not 'q'

--- weather-app/src/app/apixu.service.ts
+++ weather-app/src/app/apixu.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +10,7 @@ export class ApixuService {
 
   getWeather(location){
     return this.http.get(
-      'http://api.weatherstack.com/current?access_key=7f706c352ebb493c90765a3c11d2c0ea&q=' + location
+      'http://api.weatherstack.com/current?access_key=7f706c352ebb493c90765a3c11d2c0ea&query=' + location
     );
   }
 }
